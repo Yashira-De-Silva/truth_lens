afbad68cc85de82ac69e49e05de317d4d6903ee0
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-01-01 20:16:00 +0530

    backend

---
+++ backend/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ExampleController
+{
+    public function index()
+    {
+        return "Example Controller - install full Laravel to use controllers properly.";
+    }
+}
