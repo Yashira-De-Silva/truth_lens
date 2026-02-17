660c24e193ed04a2985189cec92b2aa6142488f7
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-02-17 19:13:29 +0530

    %

--- apps/frontend/lib/main.dart
+++ apps/frontend/lib/main.dart
@@ -25,7 +25,7 @@ class _TruthLensAppState extends ConsumerState<TruthLensApp> {
     
     return MaterialApp(
       title: 'TruthLens', 
-      debugShowCheckedModeBanner: false, //banner
+      debugShowCheckedModeBanner: false, 
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: _themeMode,
