cfb4199c18f0110eabb25c2dfb8886fdaa7c8784
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-20 21:01:26 +0530

    4

--- apps/backend/config/app.php
+++ apps/backend/config/app.php
@@ -53,18 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
-    |
-    */
-
+    
     'timezone' => 'UTC',
 
 
