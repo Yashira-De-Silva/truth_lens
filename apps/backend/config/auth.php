a89b9f5391413251094dff5bc1f15b6a1eff1df4
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-20 21:03:01 +0530

    9

--- apps/backend/config/auth.php
+++ apps/backend/config/auth.php
@@ -2,17 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Defaults
-    |--------------------------------------------------------------------------
-    |
-    | This option defines the default authentication "guard" and password
-    | reset "broker" for your application. You may change these values
-    | as required, but they're a perfect start for most applications.
-    |
-    */
-
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'api'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
