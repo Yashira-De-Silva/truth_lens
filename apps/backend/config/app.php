353c2c66c02e76ca16849f0c78c3c674269bc7c7
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-20 21:02:02 +0530

    6

--- apps/backend/config/app.php
+++ apps/backend/config/app.php
@@ -28,17 +28,6 @@ return [
 
     'env' => env('APP_ENV', 'production'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
-
     'debug' => (bool) env('APP_DEBUG', false),
 
     'url' => env('APP_URL', 'http://localhost'),
