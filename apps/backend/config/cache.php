58f18908098b8968de2e792859121d2ba1cf8163
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-20 21:05:02 +0530

    16

--- apps/backend/config/cache.php
+++ apps/backend/config/cache.php
@@ -5,21 +5,6 @@ use Illuminate\Support\Str;
 return [
     'default' => env('CACHE_STORE', 'database'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Stores
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the cache "stores" for your application as
-    | well as their drivers. You may even define multiple stores for the
-    | same cache driver to group types of items stored in your caches.
-    |
-    | Supported drivers: "array", "database", "file", "memcached",
-    |                    "redis", "dynamodb", "octane",
-    |                    "failover", "null"
-    |
-    */
-
     'stores' => [
 
         'array' => [
