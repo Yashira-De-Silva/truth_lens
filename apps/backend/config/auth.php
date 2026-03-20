a28586d202bf444d478ac576e8aad997b1d651a0
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-20 21:04:07 +0530

    12

--- apps/backend/config/auth.php
+++ apps/backend/config/auth.php
@@ -25,10 +25,6 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
