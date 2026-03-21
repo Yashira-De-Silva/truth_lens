38fc7bcc9a77ea5e659ef41e98eb788534fbc2a6
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-21 21:50:39 +0530

    6

--- apps/backend/config/session.php
+++ apps/backend/config/session.php
@@ -5,18 +5,6 @@ use Illuminate\Support\Str;
 return [
     'driver' => env('SESSION_DRIVER', 'database'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Session Lifetime
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the number of minutes that you wish the session
-    | to be allowed to remain idle before it expires. If you want them
-    | to expire immediately when the browser is closed then you may
-    | indicate that via the expire_on_close configuration option.
-    |
-    */
-
     'lifetime' => (int) env('SESSION_LIFETIME', 120),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
