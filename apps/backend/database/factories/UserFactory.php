00c23f66d47e438ebff083bee9b37f7cd7555108
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-03-22 21:43:42 +0530

    6

--- apps/backend/database/factories/UserFactory.php
+++ apps/backend/database/factories/UserFactory.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
      * @return array<string, mixed>
      */
     public function definition(): array
@@ -22,9 +26,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
