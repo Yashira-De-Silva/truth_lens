c596ef4de18d34013c2861bc99c8273f891788dd
Yashira-De-Silva <yashiradesilva@gmail.com>
2026-05-08 07:29:50 +0530

    queue status to send

--- apps/backend/app/Http/Controllers/AuthController.php
+++ apps/backend/app/Http/Controllers/AuthController.php
@@ -266,9 +266,9 @@ class AuthController extends Controller
             ]
         );
 
-        // Send the OTP via email in the background (Async)
+        // Send the OTP via email
         try {
-            Mail::to($email)->queue(new OtpMail($otp));
+            Mail::to($email)->send(new OtpMail($otp));
         } catch (\Exception $e) {
             // Log error but continue for now, or you can return error
             \Log::error("Failed to send OTP email: " . $e->getMessage());
